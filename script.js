27fe785eebc45ea7144923de86a5c108e8d8418e
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-11 15:05:07 +0545

    1st commit

---
+++ script.js
@@ -0,0 +1,33 @@
+console.log("Ashish")
+
+
+const a = `Mathematics`
+
+console.log(a.slice(3))
+
+console.log(a.split(""))
+
+console.log(a.toUpperCase())
+
+console.log(a.split("e"))
+const num = 43.76
+
+console.log(Number.parseInt(num))
+
+console.log(Math.PI)
+console.log(Math.trunc(Math.PI))
+console.log(Math.pow(5, 7))
+console.log(Math.floor(4.3))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.3))
+console.log(Math.min(2, 56, 78, 0.5))
+console.log(Math.min(2, 56, 78, 0.5))
+console.log(Math.floor(Math.random() * 10) + 1)
+
+
+
+//CODING CHALLENGE 1 
+//Print random character from your name
+const names = `Ashish`
+
+console.log(names.charAt(Math.floor(Math.random() * names.length)))
