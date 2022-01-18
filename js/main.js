1a4e512e8a60bd38c760364923cc9a764dbaabb1
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-18 10:01:29 +0545

    day 5

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-const view = document.querySelector("#view2")
-const div = view.querySelector("div")
-const h2 = div.querySelector("h2")
-
-
 //syntax: addEventListener(event, function, useCapture)
 
 
@@ -26,13 +21,14 @@ h2.addEventListener("click", (event) => {
 
 
 
-
+/*
 document.addEventListener("readystatechange", (event) => {
     if (event.target.readyState === "complete") {
         console.log("readyState: complete")
         initApp()
     }
 })
+
 
 const initApp = () => {
     const view = document.querySelector("#view2")
@@ -41,16 +37,62 @@ const initApp = () => {
 
     view.addEventListener("click", (event) => {
         // event.stopPropagation()
-        view.style.backgroundColor = "purple"
+        view.classList.toggle("purple")
+        view.classList.toggle("darkblue")
     }, false)
 
     div.addEventListener("click", (event) => {
         // event.stopPropagation()
-        div.style.backgroundColor = "blue"
+        view.classList.toggle("blue")
+        view.classList.toggle("black")
     }, false)
 
     h2.addEventListener("click", (event) => {
         // event.stopPropagation()
-        event.target.textContent = "Clicked"
+        const myText = event.target.textContent
+        myText == "My 2nd view" ?
+            (event.targgrt.textContent = "Clicked") : (event.target.textContent = "My 2nd view")
     }, false)
+
+    const nav = document.querySelector("nav")
+    nav.addEventListener("mouseover", (event) => {
+        event.target.classList.add("height100")
+    })
+
+    nav.addEventListener("mouseout", (event) => {
+        event.target.classList.remove("height100")
+    })
+
 }
+*/
+
+
+
+//FORM EVENTS HANDLING
+
+/*
+document.addEventListener("readystatechange", (event) => {
+    if (event.target.readyState === "complete") {
+        console.log("readyState: complete")
+        initApp()
+    }
+})
+
+const initApp = () => {
+    const view3 = document.querySelector("#view3")
+    const myForm = view3.querySelector("myForm")
+    myForm.addEventListener("submit", (event) => {
+        event.preventDefault()
+        console.log("submit event")
+    })
+}
+*/
+
+
+/*web storage api
+not part of the DOM - refers to the window API
+Available to JS via the global variable: Window
+
+we do not have to type window. It is implied:
+
+*/
