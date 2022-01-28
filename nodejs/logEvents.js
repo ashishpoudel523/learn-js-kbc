173a5e48ddc796ff2ccfbcee8fb22aec8234856f
Ashish Poudel <ashishpoudel523@gmail.com>
2022-01-28 17:01:22 +0545

    log events

--- nodejs/logEvents.js
+++ nodejs/logEvents.js
@@ -2,27 +2,32 @@ const {
     format
 } = require('date-fns')
 
-
-
 const {
     v4: uuid
 } = require('uuid')
 
-const fs = reqiure('fs')
 
+const fs = require('fs')
 const fsPromises = require('fs').promises
+const path = require('path')
 
-const path = require('path');
+const logEvents = async (message) => {
+    const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`
 
-const logEvents = asyc(message) => {
-        const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}
+    const logItem = `${dateTime}\t${uuid()}\t${message}\n`
 
     console.log(logItem)
-    try{
-        await fsPromses.appendFile(path.join(__dirname, '))
-    }catch {err} {
-        console.err(err)
+
+    try {
+        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname, 'logs'))
+        }
+        //testing
+        await fsPromises.appendFile(path.join(__dirname, 'logs', 'eventLog.txt'), logItem)
+    } catch (err) {
+        console.log(err)
     }
 }
 
-module.exports = logEvents;
+
+module.exports = logEvents
