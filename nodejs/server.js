f1a0f7ea0b58f7196c5f78f65f55c218817205b9
Ashish Poudel <ashishpoudel523@gmail.com>
2022-02-06 16:19:15 +0545

    middlewares

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require('express')
 const app = express()
-const path = require('path');
+const path = require('path')
+const {
+    logger
+} = require('./middleware/logEvents')
 
-const PORT = process.env.PORT || 3500;
+const PORT = process.env.PORT || 3500
 
-app.get('/', (req, res) => {
+//custom middleware logger
+app.use(logger)
+//built-in middleware to handle urlencoded data
+//in other words, form data:
+//'content-type: application/x-www-form-urlencoded'
+app.use(express.urlencoded({
+    extended: false
+}))
+
+//built-in middleware for json
+app.use(express.json())
+
+//serve static files
+app.use(express.static(path.join(__dirname, '/public')))
+
+
+
+app.get('^/$|/index(.html)?', (req, res) => {
     /*
     res.sendFile('./views/index.html', {
         root: __dirname
@@ -14,8 +34,44 @@ app.get('/', (req, res) => {
 })
 
 
-app.get('/new-page.html', (req, res) => {
+app.get('/new-page(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
+})
+
+app.get('/old-page(.html)?', (req, res) => {
+    res.redirect(301, '/new-page.html') //302 by default
+})
+
+
+//Route Handlers
+app.get('/hello(.html)?', (req, res, next) => {
+    console.log('attempted to load hello.html')
+    next()
+}, (req, res) => {
+    res.send('Hello World!')
+})
+
+
+//chaining route handlers 
+const one = (req, res, next) => {
+    console.log('one')
+    next()
+}
+
+const two = (req, res, next) => {
+    console.log('two')
+    next()
+}
+
+const three = (req, res) => {
+    console.log('three')
+    res.send('Finished!')
+}
+
+app.get('/chain(.html)?', [one, two, three])
+
+app.get('/*', (req, res) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'))
 })
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
