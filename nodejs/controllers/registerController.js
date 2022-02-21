8d77f51334188fd99e2ede8c0614c087610255e5
Ashish Poudel <ashishpoudel523@gmail.com>
2022-02-21 20:57:59 +0545

    mongoose

--- nodejs/controllers/registerController.js
+++ nodejs/controllers/registerController.js
@@ -1,20 +1,14 @@
-const User = require('../model/User')
+const User = require('../model/User');
 const bcrypt = require('bcrypt');
 
 const handleNewUser = async (req, res) => {
-    const {
-        user,
-        pwd
-    } = req.body;
-    if (!user || !pwd) return res.status(400).json({
-        'message': 'Username and password are required.'
-    });
+    const { user, pwd } = req.body;
+    if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
 
     // check for duplicate usernames in the db
-    const duplicate = await User.findOne({
-        username: user
-    }).exec()
+    const duplicate = await User.findOne({ username: user }).exec();
     if (duplicate) return res.sendStatus(409); //Conflict 
+
     try {
         //encrypt the password
         const hashedPwd = await bcrypt.hash(pwd, 10);
@@ -25,18 +19,12 @@ const handleNewUser = async (req, res) => {
             "password": hashedPwd
         });
 
-        console.log(result)
+        console.log(result);
 
-        res.status(201).json({
-            'success': `New user ${user} created!`
-        });
+        res.status(201).json({ 'success': `New user ${user} created!` });
     } catch (err) {
-        res.status(500).json({
-            'message': err.message
-        });
+        res.status(500).json({ 'message': err.message });
     }
 }
 
-module.exports = {
-    handleNewUser
-};
+module.exports = { handleNewUser };
